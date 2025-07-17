cb82cad700ee37cdf3f508fb850960e72f3e7a34
visheshkumar07 <visheshkumar12045@gmail.com>
2025-07-17 19:30:22 +0530

    Add Project Files

---
+++ controllers/users.js
@@ -0,0 +1,48 @@
+const User = require("../models/user")
+
+module.exports.renderSignupForm = (req, res) =>{
+    res.render("users/signup.ejs");
+}
+
+
+module.exports.signup = async(req, res) =>{
+    try{
+        let {username, email, password} = req.body;
+        const newUser = new User({email, username});
+        const registeredUser = await User.register(newUser, password);
+        console.log(registeredUser);
+        req.login(registeredUser, (err)=>{
+            if(err){
+                next(err);
+            }
+             req.flash("sucess", "Welcome to Wanderlust");
+             res.redirect("/listings");
+        });
+       
+    }catch(e){
+        req.flash("error", e.message);
+        res.redirect("/signup");
+    }
+    
+}
+
+
+module.exports.renderLoginForm = (req, res) =>{
+    res.render("users/login.ejs");
+}
+
+module.exports.login = async(req, res) =>{
+    req.flash("sucess","Welcome back to Wanderlust!");
+    let redirectUrl = res.locals.redirectUrl || "/listings";
+    res.redirect(redirectUrl);
+}
+
+module.exports.logout = (req, res, next) =>{
+    req.logOut((err)=>{
+        if(err){
+            return next(err);
+        }
+        req.flash("sucess", "You are logged out!");
+        res.redirect("/listings");
+    });
+}
